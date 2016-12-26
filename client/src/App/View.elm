60ff135aba607006b1e283654455484b511d578d
Mark Huo <huomian@outlook.com>
2016-12-26 09:54:04 +0800

    basic nav

--- client/src/App/View.elm
+++ client/src/App/View.elm
@@ -2,14 +2,27 @@ module App.View exposing (view)
 
 import App.Model exposing (Page(..), Model)
 import App.Update exposing (Msg(..))
-import Html exposing (Html, div)
+import Html exposing (Html, div, a, text)
+import Html.Events exposing (onClick)
 import Pages.PageNotFound.View
 import Pages.Welcome.View
 
 
 view : Model -> Html Msg
 view model =
-    div [] [ viewMainContent model ]
+    div []
+        [ viewHeader model
+        , viewMainContent model
+        ]
+
+
+viewHeader : Model -> Html Msg
+viewHeader model =
+    div []
+        [ a [ onClick <| SetActivePage Welcome ] [ text "welcome" ]
+        , text "|"
+        , a [ onClick <| SetActivePage PageNotFound ] [ text "404" ]
+        ]
 
 
 viewMainContent : Model -> Html Msg
